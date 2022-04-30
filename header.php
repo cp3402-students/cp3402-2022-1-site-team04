e2c58eea73fd658c6cf6a72e3e81934b50f8f5f0
Liam Williams <liam.williams@my.jcu.edu.au>
2022-04-30 11:30:02 +0800

    add header

---
+++ header.php
@@ -0,0 +1,17 @@
+<header>
+<div class="headercontainer">
+        <ul class="nav">
+            <li>        
+                <a href="index.html">
+                    <img  src="screenshot.jpg" width="80" height="80"></a>
+            </li>
+           <li><a href="index.html">Home</a></li>
+            <li><a href="about.html">Programs</a></li>
+            <li><a href="contacts.html">Location</a></li>
+        </ul>
+        <h1>Townsville Jazz Club</h1>
+            
+        </div>
+</header>
+
+
