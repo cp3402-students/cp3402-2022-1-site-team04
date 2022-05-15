30ed7b45ca7aa365d69437c174c57c61d1d1d006
Liam Williams <liam.williams@my.jcu.edu.au>
2022-05-15 14:45:16 +0800

    header fixed

--- header.php
+++ header.php
@@ -10,47 +10,133 @@
 
 .headercontainer{
     align-items: center;
-    width: 100%;
+    float: right;
+    width:90%;
+    padding-left: 10%;
+    height: 15%;
+}
+
+nav{
+    align-items: right;
+    padding-bottom: 20%;
 }
 
 li{
 font: bold;
+float: center;
+text-align: center;
+
 }
 
 ul li{
+    text-decoration: none;
     list-style: none;
     display: inline-block;
     float: none;
-    padding-right: 5%;
-
+    /*padding-right: 5%;*/
+    /*padding-top: 5%;*/
+    /*padding-bottom: 5%;*/
+    width: 15%;
+    /*margin-left: 1%;
+    margin-right: 1%;*/
+    height: 80%;
 }
+
 
 ul li a {
+    /*display: block;
     text-decoration: none;
     color: black;
-    float: center;
-    /*padding-right:15%;*/
+    float: top ;
+    padding-right:15%;
     font-weight: bold;
     font-family: Arial, Helvetica, sans-serif;
+    weight: 1;
+    width: 20%;
+    vertical-align: middle;
+    line-height: 1.5;
+    display: inline-block;
+    padding-top: 43%;
+    padding-bottom: 43%;*/
+}
 
+.listedLinks {
+    display: block;
+    text-decoration: none;
+    color: black;
+    float: top ;
+    font-weight: bold;
+    font-family: Arial, Helvetica, sans-serif;
+    weight: 1;
+    vertical-align: middle;
+    line-height: 1.5;
+    display: inline-block;
+    padding-top: 23%;
+    padding-bottom: 23%;
+    width: 100%;
+}
+
+.listedImage{
+    display: block;
+    text-decoration: none;
+    color: black;
+    float: top ;
+    font-weight: bold;
+    font-family: Arial, Helvetica, sans-serif;
+    weight: 1;
+    vertical-align: middle;
+    line-height: 1.5;
+    display: inline-block;
     
 }
+
+ul li a:hover {
+    color:white;
+    background-color: gray;
+}
+ul li :hover {
+    background-color: gray;
+}
+
+
+.headerFormat{
+    padding-bottom: 5%;
+}
+
+.iconformat{
+    margin-top: 5%;
+    float: left;
+}
+.menuformat{
+    float: right;
+}
+.full{
+    width: 100%;
+}
+
+
+.right{
+    float: right;
+    width: 100%;
+}
+
     </style>
+
     
-    <link rel ="stylesheet" href="style.css">
+   
 <div class="headercontainer">
-        <ul class="nav">
-            <li>        
-                <a href="index.html">
-                    <img  src="screenshot.jpg" alt="Jazz logo" width="80" height="80"></a>
-            </li>
-           <li><a href="index.html">HOME</a></li>
-            <li><a href="about.html">PROGRAMS</a></li>
-            <li><a href="contacts.html">LOCATION</a></li>
-            <li><a href="contacts.html">PHOTOS</a></li>
-            <li><a href="contacts.html">JOIN</a></li> 
+    <nav class="full">
+
+   <div class = "right"> 
+        <ul class="nav">   
+            <li><a class ="listedImage" href="index.html"><img href="index.html" src='http://localhost/wordpress/wp-content/uploads/2022/05/jazzman-small.jpg'/></a></li>
+           <li><a class ="listedLinks" href="index.html">HOME</a></li>
+            <li><a class ="listedLinks" href="about.html">PROGRAMS</a></li>
+            <li><a class ="listedLinks" href="contacts.html">LOCATION</a></li>
+            <li><a class ="listedLinks" href="contacts.html">PHOTOS</a></li>
+            <li><a class ="listedLinks" href="contacts.html">JOIN</a></li> 
         </ul>
-        <h1>Townsville Jazz Club</h1>
-            
-        </div>
+</div>
+    </nav>        
+</div>
 </header>
