da7d1fab464faa922f84f88f195f1d9274320221
DGMGIT <daniel.mackenzie1@my.jcu.edu.au>
2022-05-24 13:21:42 +1000

    add basic footer

--- theme/jazz-child/functions.php
+++ theme/jazz-child/functions.php
@@ -1,15 +1,49 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-function my_theme_enqueue_styles() {
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+function my_theme_enqueue_styles()
+{
     $parenthandle = 'parent-style'; // This is 'understrap-style' for the understrap theme.
     $theme = wp_get_theme();
-    wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css',
+    wp_enqueue_style($parenthandle, get_template_directory_uri() . '/style.css',
         array(),  // if the parent theme code has a dependency, copy it to here
         $theme->parent()->get('Version')
     );
-    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
-        array( $parenthandle ),
+    wp_enqueue_style('child-style', get_stylesheet_uri(),
+        array($parenthandle),
         $theme->get('Version') // this only works if you have Version in the style header
     );
 }
+
+/*
+* Define a constant path to our single template folder
+*/
+define(SINGLE_PATH, TEMPLATEPATH . '/single');
+
+/**
+ * Filter the single_template with our custom function
+ */
+add_filter('single_template', 'my_single_template');
+
+/**
+ * Single template function which will choose our template
+ */
+function my_single_template($single)
+{
+    global $wp_query, $post;
+
+    /**
+     * Checks for single template by category
+     * Check by category slug and ID
+     */
+    foreach ((array)get_the_category() as $cat) :
+
+        if (file_exists(SINGLE_PATH . '/single-cat-' . $cat->slug . '.php'))
+            return SINGLE_PATH . '/single-cat-' . $cat->slug . '.php';
+
+        elseif (file_exists(SINGLE_PATH . '/single-cat-' . $cat->term_id . '.php'))
+            return SINGLE_PATH . '/single-cat-' . $cat->term_id . '.php';
+
+    endforeach;
+}
+
