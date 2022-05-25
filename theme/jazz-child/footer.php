09c69f430e4ac24a1575c18ead3d4fbf3ca6a195
Sandeepkaur19 <62980505+Sandeepkaur19@users.noreply.github.com>
2022-05-25 22:13:14 +1000

    update footer

--- theme/jazz-child/footer.php
+++ theme/jazz-child/footer.php
@@ -1,7 +1,42 @@
 <!DOCTYPE html>
 <html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+	<title> Document </title>
+</head>
+<body>
+	<div class="footer-container">
+		<div class="footer">
+			<div class="footer-heading footer-1"></div>
+				<h2> About Us </h2>
+        </div>
+        <div class="footer">
+			<div class="footer-heading footer-2"></div>
+				<h2> Contact Us </h2>
+                <a href ="#"> Support </a>
+                <a href ="#"> Contact </a>
+        </div>
+        <div class="footer">
+			<div class="footer-heading footer-3"></div>
+				<h2> Social Media </h2>
+                <a href ="#"> Facebook</a>
+                <a href ="#"> Instragram</a>
+        </div>
+        <div class="footer-email-form">
+            <h2> Join Our NewsLetter </h2>
+            <input type="email" placeholder="Enter your email 
+            address" id ="footer-email">
+            <input type="submit" value="Sign Up"
+            id="footer-email-btn">
+        </div>
+    </div>
+</body>
+</html>
 
 <head>
+    
 <style>
 
 #colophon {
